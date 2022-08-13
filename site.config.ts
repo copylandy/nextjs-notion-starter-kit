8f4e56121c71ae9f6abd90a00d0aa8f210b1b0f0
copylandy <111176887+copylandy@users.noreply.github.com>
2022-08-13 18:33:26 -0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '702521cebe424226a93d1ffb0c2614eb',
+  rootNotionPageId: 'ac5be60666e8431d9e068ac652e13a37',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
