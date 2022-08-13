bfc6b1767ce44a517fe9810b0b081dceeaa09aff
copylandy <111176887+copylandy@users.noreply.github.com>
2022-08-13 07:10:04 -0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://spiralfeed.xyz/',
+  author: 'spiralfeed',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'spiralfeed',
+  github: 'spiralfeed',
+  linkedin: 'spiralfeed',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
